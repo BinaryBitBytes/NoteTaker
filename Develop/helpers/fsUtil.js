52fa7a43b6b1451c2422111d5948aaa5bf8e1b3e
BinaryBitBytes <94703967+BinaryBitBytes@users.noreply.github.com>
2022-03-19 17:01:58 -0400

    built out FSUTIL.js

--- Develop/helpers/fsUtil.js
+++ Develop/helpers/fsUtil.js
@@ -11,3 +11,17 @@ const writeIt = (destination,content) =>
     fs.writeIt(destination, JSON.stringify(content, null, 4), (err) =>
     err ? console.error(err) : console.info(`\n Wrote to ${destination}`)
     );
+
+const readItUpdateIt = (content, file) => {
+    fs.readFile(file, 'utf8', (err,data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const toJSON = JSON.parse(data);
+            toJSON.push(content);
+            writeToFile(file, toJSON);
+        }
+    });
+};
+
+module.exports = {readIt, writeIt, readItUpdateIt};
