af941b0c4e60bbdcc448879b46b6256603efc09b
BinaryBitBytes <94703967+BinaryBitBytes@users.noreply.github.com>
2022-03-19 16:03:34 -0400

    attempting to create the midware and midware js from using instructor and course material as a moch up

--- Develop/server.js
+++ Develop/server.js
@@ -1,20 +1,41 @@
 // dependancy for express
 const express = require('express');
+const path = require('path');
 // dependancy for  FS for saving files
 const fs = require('fs');
 // making a route that points to index.js
 const route = ('./routes/index.js');
+const { midWare } = require('./middleware/midWare');
 
 //communication port 3001 for local host
 const PORT = 3001;
 // using express as a function
 const applet = express();
+app.use(midware);
+
+const middleware = (req, res, next) => {
+    // ANSI escape code that instructs the terminal to print in yellow
+    const yellow = '\x1b[33m%s\x1b[0m';
+    // Log out the request type and resource
+    console.log(yellow, `${req.method} request to ${req.path}`);
+    // Built-in express method to call the next middleware in the stack.
+    next();
+  };
+app.use(middleware);
+app.get('/', (req, res) => res.json(`GET route`));
+app.post('/', (req, res) => res.json(`POST route`));
+app.put('/:id', (req, res) => res.json(`PUT route`));
+app.delete('/:id', (req, res) => res.json(`DELETE route`));
+app.patch('/:id', (req, res) => res.json(`PATCH route`));
+app.listen(PORT, () =>
+  console.log(`Listening for requests on port ${PORT}! 🏎️`)
+);
 
 //Building out the Middleware for our JSON and data
 app.use(express.json()); //using express and parsing the contents into a JSON object
 //app.use(express.urlencoded({ extended: true}));
 app.use('api', applet);
-
+console.log(``);
 app.use(express.static('public')); //creating a static rout for the public folder
 
 // GET THE ROUTE for our main page
