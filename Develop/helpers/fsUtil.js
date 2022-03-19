b3a078f84548812b25c19a39efb7fe9b6075abdd
BinaryBitBytes <94703967+BinaryBitBytes@users.noreply.github.com>
2022-03-19 17:03:24 -0400

    built out FSUTIL.js updated

--- Develop/helpers/fsUtil.js
+++ Develop/helpers/fsUtil.js
@@ -2,11 +2,6 @@ const fs = require('fs');
 const util = require('util');
 // using fs promise
 const readIt = util.promisify(fs.readFile);
-
-@param {string} destination
-@param {objecct} content
-@returns {void}
-
 const writeIt = (destination,content) =>
     fs.writeIt(destination, JSON.stringify(content, null, 4), (err) =>
     err ? console.error(err) : console.info(`\n Wrote to ${destination}`)
