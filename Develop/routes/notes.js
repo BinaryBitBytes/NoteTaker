8461650a73f025e41e9c142a40bbec30dbc5a387
BinaryBitBytes <94703967+BinaryBitBytes@users.noreply.github.com>
2022-03-20 15:33:33 -0400

    updated the git ignore

---
+++ Develop/routes/notes.js
@@ -0,0 +1,19 @@
+const notes= (userInput) =>
+{
+    let userInput = '';
+    for (filedNote){
+        while (condition) {
+            userInputer == '';
+            window.prompt(`User Input is blank, please enter anotehr input.`);
+            continue
+        }
+    if userInput(wordsEntered) <= CharacterData.length(15) {
+        console.log(`You have reached 15 characters, this is to inform you that your note requirement is met and can be submitted.`);
+
+
+    }
+    }
+}
+module.exports = {notes} 
+
+//! WILDCARD ^^
