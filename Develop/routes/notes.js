fdf61b4670210d0782ba083450e59efe559106c1
BinaryBitBytes <94703967+BinaryBitBytes@users.noreply.github.com>
2022-03-31 15:34:08 -0400

    updated changes to the source code for routes and taks as well as notes

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,19 +1,47 @@
+const express = require('express');
+const router = require('express').Router();
+const PORT = 3001;
+const app = express();
+const { noteData } = require('./scripts/notes.js');
+//getting the route for sending a response
+app.get('/', (req, res) => {
+    res.send('http://localhost:3001/api');
+  });
+
+app.listen(PORT, () =>
+    console.log(`listening locally at http://localhost:${PORT}`)
+); 
+
+app.get('/api', (req, res) => res.json(noteData));
+
+router.post('/', async (req,res) => {
+    try{
+
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
 const notes= (userInput) =>
 {
     let userInput = '';
-    for (filedNote){
-        while (condition) {
+    for (filedNote)
+    {
+        while (filedNote) 
+        {
             userInputer == '';
-            window.prompt(`User Input is blank, please enter anotehr input.`);
-            continue
-        }
-    if userInput(wordsEntered) <= CharacterData.length(15) {
-        console.log(`You have reached 15 characters, this is to inform you that your note requirement is met and can be submitted.`);
+            window.prompt(`User Input is blank, please enter another input.`);
+            continue;
+    };
+    if (userInput.wordsEntered <= CharacterData.length(15))
+    {
+        console.log(`You have reached 15 characters, this is
+         to inform you that your note requirement is met and 
+         can be submitted.`);
 
-
-    }
-    }
-}
+    };
+    };
+};
 module.exports = {notes} 
 
 //! WILDCARD ^^
